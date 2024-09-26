85c72c8b236ba098975a43528b9138b0931f1c34
wofly98 <109505948+wofly98@users.noreply.github.com>
2024-09-26 14:45:05 +0800

    Update build_hyp_aboot.sh

--- scripts/build_hyp_aboot.sh
+++ scripts/build_hyp_aboot.sh
@@ -4,9 +4,9 @@ make -C src/qhypstub CROSS_COMPILE=aarch64-linux-gnu-
 
 # patch to reduce mmc speed as some boards have intermittent failures when
 # inititalizing the mmc (maybe due to using old/recycled flash chips)
-echo 'DEFINES += USE_TARGET_HS200_CAPS=1 WITH_LK2ND_DEVICE_2ND=1' >> src/lk2nd/project/lk1st-msm8916.mk
+echo 'DEFINES += USE_TARGET_HS200_CAPS=1' >> src/lk2nd/project/lk1st-msm8916.mk
 
-make -C src/lk2nd LK2ND_BUNDLE_DTB="msm8916-512mb-mtp.dtb" LK2ND_COMPATIBLE="yiming,uz801-v3" WITH_LK2ND_DEVICE_2ND=1 \
+make -C src/lk2nd LK2ND_BUNDLE_DTB="msm8916-512mb-mtp.dtb" LK2ND_COMPATIBLE="thwc,ufi001c" \
     TOOLCHAIN_PREFIX=arm-none-eabi- lk1st-msm8916
 
 # test sign
